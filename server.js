448639046ec0aecff1e821b75de1cf9d0cedfcb8
SharanBarfa <sharanbarfa2003@gmail.com>
2025-05-17 17:58:17 +0530

    Update server.js
    change frontend url

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 // Allow CORS from any origin in development mode
 app.use(cors({
-  origin: '*', // Allow all origins in development
+  origin: 'https://erm-client.vercel.app', // Allow all origins in development
   credentials: false, // Set to false when using '*' for origin
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
